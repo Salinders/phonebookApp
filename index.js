0f3e3c5d48619d66e895dbec78c9ba33510f6ac2
Salinders <dejentesfaldet16@gmail.com>
2024-04-15 02:10:53 +0300

    added dist

--- index.js
+++ index.js
@@ -7,6 +7,7 @@ const app = express()
 
 app.use(morgan('tiny'));
 app.use(cors())
+app.use(express.static('dist'))
 
 
 app.use(express.json())
@@ -107,7 +108,7 @@ app.post('/api/persons', (request, response)=>{
 
 })
 
-const PORT =  process.env.PORT|| 3001
+const PORT =  process.env.PORT || 3001
 app.listen(PORT, () =>{
     console.log(`Server running on port ${PORT}`)
 
